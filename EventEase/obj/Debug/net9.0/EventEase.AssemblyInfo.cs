129bf9dffb5539c789f3663e742f30c1f78cb93a
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2025-06-23 04:22:48 +0200

    Create View
    Created Views

--- EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b8e7bb3b13f84a4f7c7e5537679e1db12034e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a47fd1c4c99d5a51fc1d56976b9306e1597de43")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
